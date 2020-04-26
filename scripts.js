a0ad320e9b944618d480b4735d40df2a05348766
Cezary <cezaryjjung@gmail.com>
2020-04-26 16:52:27 +0200

    download items

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+const screen = document.getElementById('screen-display')
+
+const zeroBtn = document.getElementById('zero');
+const oneBtn = document.getElementById('one');
+const twoBtn = document.getElementById('two');
+const threeBtn = document.getElementById('three');
+const fourBtn = document.getElementById('four');
+const fiveBtn = document.getElementById('five');
+const sixBtn = document.getElementById('six');
+const sevenBtn = document.getElementById('seven');
+const eightBtn = document.getElementById('eight');
+const nineBtn = document.getElementById('nine');
+
+
+const removeBtn = document.getElementById('remove');
+const backspaceBtn = document.getElementById('backspace');
+const divideBtn = document.getElementById('divide');
+const multiplyBtn = document.getElementById('multiply');
+const minusBtn = document.getElementById('minus');
+const plusBtn = document.getElementById('plus');
+const equalsBtn = document.getElementById('equals');
+
+const numBtn = document.getElementsByClassName('button-num');
+const operatorBtn = document.getElementsByClassName('button-operator');
