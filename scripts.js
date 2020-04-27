5a6a30ca7bc7df9da9eab2184aed0a4b76fbadfd
Cezary <cezaryjjung@gmail.com>
2020-04-27 20:42:03 +0200

    add functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 
 const screen = document.getElementById('screen-display')
 
+let displayVal = '0';
+let pendingValue;
+let evalStringArray = [];
+
 const zeroBtn = document.getElementById('zero');
 const oneBtn = document.getElementById('one');
 const twoBtn = document.getElementById('two');
@@ -20,6 +24,99 @@ const multiplyBtn = document.getElementById('multiply');
 const minusBtn = document.getElementById('minus');
 const plusBtn = document.getElementById('plus');
 const equalsBtn = document.getElementById('equals');
+const dot = document.getElementById('dot');
 
 const numBtn = document.getElementsByClassName('button-num');
 const operatorBtn = document.getElementsByClassName('button-operator');
+
+const displayValue = (click) => {
+  const btnText = click.target.innerText;
+  if(displayVal === '0')
+    displayVal = '';
+
+    displayVal += btnText;
+    screen.innerText = displayVal;
+
+  }
+
+for (let i = 0; i < numBtn.length; i++) {
+    numBtn[i].addEventListener('click', displayValue, false)
+  
+}
+
+const performOperation = (click) => {
+
+  const operator = click.target.innerText;
+  switch(operator){
+    case '+':
+      pendingValue = displayVal;
+      displayVal = '0';
+      screen.innerText = displayVal;
+      evalStringArray.push(pendingValue);
+      evalStringArray.push('+');
+      break;
+
+      case '-':
+        pendingValue = displayVal;
+        displayVal = '0';
+        screen.innerText = displayVal;
+        evalStringArray.push(pendingValue);
+        evalStringArray.push('-');
+        break;
+        
+      case 'x':
+        pendingValue = displayVal;
+        displayVal = '0';
+        screen.innerText = displayVal;
+        evalStringArray.push(pendingValue);
+        evalStringArray.push('*');
+        break;
+     
+        case '÷':
+        pendingValue = displayVal;
+        displayVal = '0';
+        screen.innerText = displayVal;
+        evalStringArray.push(pendingValue);
+        evalStringArray.push('/');
+        break;
+
+        case '=':
+          evalStringArray.push(displayVal);
+          let evaluation = eval(evalStringArray.join(' '));
+          displayVal = evaluation + ' ';
+          screen.innerText = displayVal;
+          evalStringArray = [];
+          break;
+
+          default:
+          break;
+  }
+}
+
+for (let i = 0; i < operatorBtn.length; i++) {
+  operatorBtn[i].addEventListener('click', performOperation, false)
+}
+
+removeBtn.onclick = () => {
+  displayVal = '0';
+  pendingValue = undefined;
+  evalStringArray = [];
+  screen.innerText = displayVal;
+}
+
+backspaceBtn.onclick = () => {
+  let lengthDisplayValue = displayVal.length;
+  displayVal = displayVal.slice(0, lengthDisplayValue - 1);
+  
+  if(displayVal === '')
+      displayVal = '0';
+      screen.innerText = displayVal;
+
+}
+
+dot.onclick = () => {
+  if(!displayVal.includes('.'))
+      displayVal += '.';
+      screen.innerText = displayVal;
+}
+
